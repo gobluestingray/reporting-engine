16ec88873d5d1524270a91a4d6ca8f74f3cc3510
Martin Carriel <41842065+ambientstl@users.noreply.github.com>
2022-09-06 10:08:06 -0500

    [BLUE-3][UPD] Batch report printing customizations
    Changes:
     - Adds a new Server Action, Batch Print Reports, for Sale Order and Product Template models
     - Adds a new wizard, Batch Print Report Wizard, that is launched by the Server Action
       - All HTML reports for the model are available for batching
       - Adds the option to collate reports by record instead of report

--- report_batch/__manifest__.py
+++ report_batch/__manifest__.py
@@ -10,8 +10,14 @@
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Reporting",
     "website": "https://github.com/OCA/reporting-engine",
-    "depends": ["stock"],
-    "data": ["security/ir.model.access.csv", "views/ir_action_report_view.xml"],
+    "depends": ["sale", "stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/form_batch_print_report_wizard.xml",
+        "views/ir_action_report_view.xml",
+        "views/report_batch_template.xml",
+        "views/action_batch_print_report.xml",
+    ],
     "installable": True,
     "maintainers": ["bodedra"],
 }
--- report_batch/models/__init__.py
+++ report_batch/models/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import batch_print_reports_wizard
 from . import ir_action_report
